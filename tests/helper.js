b389348527bf7708db2d0dd3dc52e61d833d4826
Dawid-Oskwarek <dawidoskwarek97@gmail.com>
2022-07-26 21:54:57 +0100

    test

--- tests/helper.js
+++ tests/helper.js
@@ -5,9 +5,11 @@ const opts = {
     silent: false
 };
 
+const token = process.env.TOKEN;
+
 export const actCmd = (jobName) => {
     console.log(`Running Command: act -j ${jobName} -P ubuntu-latest=ghcr.io/catthehacker/ubuntu:act-latest`);
-    const exec = sh.exec(`act -j ${jobName} -P ubuntu-latest=ghcr.io/catthehacker/ubuntu:act-latest`, opts);
+    const exec = sh.exec(`act -j ${jobName} -P ubuntu-latest=ghcr.io/catthehacker/ubuntu:act-latest -s TOKEN=${token}`, opts);
     if (exec.code !== 0) {
         console.error('WARNING');
         console.error(`Non-zero exit code. Code was ${exec.code}`);
