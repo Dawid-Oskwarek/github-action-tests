be63532759afa7dc40a5b75d8b29c0483e0021ef
Dawid-Oskwarek <109984987+Dawid-Oskwarek@users.noreply.github.com>
2023-01-04 23:28:43 +0000

    Update example.js

--- tests/example.js
+++ tests/example.js
@@ -2,7 +2,7 @@ import { expect } from 'chai';
 import { actCmd } from './helper.js';
 
 describe('Github Action Test - Example', () => {
-    it('should check for broken links on the provided url', async () => {
+    it('should run an action successfully', async () => {
         const actOutput = actCmd('exampleJob');
         expect(actOutput).to.not.include('Failure');
     });
